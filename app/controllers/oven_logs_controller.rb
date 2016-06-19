2f845f66c54233057eea927a4a0c6e4a3f4cef64
josephferrairo <josephferrairo@gmail.com>
2016-06-19 13:01:55 -0400

    added user login links in application.html.erb, added wice grid to index.html.erb

--- app/controllers/oven_logs_controller.rb
+++ app/controllers/oven_logs_controller.rb
@@ -1,6 +1,10 @@
 class OvenLogsController < ApplicationController
   def index
     @oven_log = OvenLog.all
+    @oven_logs_grid = initialize_grid(OvenLog,
+    order: 'oven_logs.customer',
+    order_direction: 'asc'
+    )
   end
 
   def new
@@ -8,9 +12,10 @@ class OvenLogsController < ApplicationController
   end
 
   def create
-    @oven_log = current_user.oven_log.create(oven_log_params)
+    @oven_log = current_user.oven_logs.create(oven_log_params)
     if @oven_log.save
       flash[:success] = 'New log created!'
+      redirect_to oven_logs_path
     else
       render :new, :status => :unprocessable_entity
     end
