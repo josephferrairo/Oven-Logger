12d356310dd4878ed79e6b573ff97e5aacf04f32
josephferrairo <josephferrairo@gmail.com>
2016-06-20 23:49:24 -0400

    fixed her_in

--- app/models/oven_log.rb
+++ app/models/oven_log.rb
@@ -7,7 +7,7 @@ class OvenLog < ActiveRecord::Base
   validates :operator, presence: :true
   validates_presence_of :stress_asset, :if => :stress_in
   validates_presence_of :her_asset, :if => :her_in
-  validates_datetime :her_in, :after => :her_in, :allow_blank => true
+  validates_datetime :her_out, :after => :her_in, :allow_blank => true
   validates_datetime :stress_out, :after => :stress_in, :allow_blank => true
-  
+
 end
