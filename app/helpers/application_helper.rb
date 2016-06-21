933c43305817f7acaada0f861d5211b624baf673
josephferrairo <josephferrairo@gmail.com>
2016-06-20 23:35:53 -0400

    adjusted datetime for application, cleaned up whitespace in index.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Date
+# ----------------------------
+Date::DATE_FORMATS[:default] = "%m/%d/%y"
+
+# DateTime / Time
+# ----------------------------
+Time::DATE_FORMATS[:default] = "%m/%d/%y %I:%M %P"
 end
