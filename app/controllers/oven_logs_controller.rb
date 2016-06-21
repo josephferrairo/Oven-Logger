394d5f25aa0a7e13cb655e01b27b7650a737137f
josephferrairo <josephferrairo@gmail.com>
2016-06-21 00:59:52 -0400

    added edit, update, and delete

--- app/controllers/oven_logs_controller.rb
+++ app/controllers/oven_logs_controller.rb
@@ -26,6 +26,26 @@ class OvenLogsController < ApplicationController
     @oven_log = OvenLog.find(params[:id])
   end
 
+  def edit
+    @oven_log = OvenLog.find(params[:id])
+  end
+
+  def update
+    @oven_log = OvenLog.find(params[:id])
+    @oven_log.update_attributes(oven_log_params)
+    if @oven_log.valid?
+      redirect_to oven_logs_path
+    else
+      render :edit, :status => :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @oven_log = OvenLog.find(params[:id])
+    @oven_log.destroy
+    redirect_to root_path
+  end
+
   private
   def oven_log_params
     params.require(:oven_log).permit(:customer, :work_order, :purchase_order, :stress_in, :stress_out,  :stress_asset, :her_in, :her_out, :her_asset, :operator)
