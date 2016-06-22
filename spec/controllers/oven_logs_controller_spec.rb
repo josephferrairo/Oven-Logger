d2dcf5c300f2380555604783342a4cd5d3f9bf19
josephferrairo <josephferrairo@gmail.com>
2016-06-21 22:23:00 -0400

    added show http status 200 test

--- spec/controllers/oven_logs_controller_spec.rb
+++ spec/controllers/oven_logs_controller_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe OvenLogsController, type: :controller do
     end
   end
 
+  describe "GET #show" do
+    it "returns a successful http status code" do
+      oven_log = Fabricate(:oven_log)
+
+      get :show, id: oven_log.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "get #new" do
+
+    it "returns a successful http status code" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
